3081ae994b1453345a0a5fa906fd5d0f233ff57f
madison24 <mhousman24@gmail.com>
2023-09-10 17:32:56 -0500

    update index

--- index.js
+++ index.js
@@ -14,3 +14,8 @@ app.get("/", (req, res) => {
 app.listen(8080, () => {
   console.log("Your app is listening on port 8080.");
 });
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
