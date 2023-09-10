eec8a08f652eeea4b8d48c6674940714884122b1
madison24 <mhousman24@gmail.com>
2023-09-10 16:50:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 const express = require("express");
 const app = express();
+
+app.get("/movies", (req, res) => {
+  res.json(topTenBooks);
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to myFlix");
+});
+
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
+});
